28ee52518c12872f7d25dfb4b2482309361209e4
gayathri <142076003+space-monkey22@users.noreply.github.com>
2024-12-30 21:24:30 +0530

    Update page.js

--- app/profile/page.js
+++ app/profile/page.js
@@ -1,4 +1,4 @@
-'use client'
+use client'
 import React, { useEffect, useState } from 'react';
 import { db } from '@/firebase/firebaseinit';
 import { collection, doc, getDoc, getDocs, query, where } from 'firebase/firestore';
